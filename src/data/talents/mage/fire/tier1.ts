d7bf23b72edb4ec758c2f2fc1be11fe40f3bbd2d
Djimo van Berlo <djimovanberlo@gmail.com>
2021-08-26 18:13:12 +0200

    Update tier1.ts

--- src/data/talents/mage/fire/tier1.ts
+++ src/data/talents/mage/fire/tier1.ts
@@ -1,16 +1,7 @@
 import { Talent } from '../../Classes'
 
 export default [
-  new Talent(
-    'Fire Warding',
-    'spell_fire_firearmor',
-    ['Increases your Fire resistance by ', ' and gives your Fire Ward a ', '% chance to reflect Fire spells and effects while active.'],
-    2,
-    [
-      [20, 40],
-      [10, 20],
-    ]
-  ),
+  null,
   new Talent('Improved Fireball', 'spell_fire_flamebolt', ['Reduces the casting time of your Fireball spell by ', ' sec.'], 5, [
     [0.1, 0.2, 0.3, 0.4, 0.5],
   ]),
