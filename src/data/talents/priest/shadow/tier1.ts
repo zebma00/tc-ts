9e238e9a896a0f73b8dd42de50fc73c0e69988f3
Djimo van Berlo <djimovanberlo@gmail.com>
2021-08-27 12:15:58 +0200

    Update tier1.ts

--- src/data/talents/priest/shadow/tier1.ts
+++ src/data/talents/priest/shadow/tier1.ts
@@ -2,16 +2,7 @@ import { Talent } from '../../Classes'
 
 export default [
   null,
-  new Talent(
-    'Spirit Tap',
-    'spell_shadow_requiem',
-    [
-      'Gives you a ',
-      '% chance to gain a 100% bonus to your spirit after an enemy that yields experience or honor is killed while effected by your Shadow Word: Pain. For the duration, your mana will regenerate at a 50% rate while casting. Lasts 15 sec.',
-    ],
-    5,
-    [[20, 40, 60, 80, 100]]
-  ),
+
   new Talent('Blackout', 'spell_shadow_gathershadows', ['Your Shadow spells have a .', '% chance to stun the target for 3 sec.'], 5, [
     [2, 4, 6, 8, 10],
   ]),
