72a20562a98b8aebaa8e82efd4aa32de04baee88
Djimo van Berlo <djimovanberlo@gmail.com>
2021-08-30 13:48:12 +0200

    Update tier2.ts

--- src/data/talents/hunter/beast-mastery/tier2.ts
+++ src/data/talents/hunter/beast-mastery/tier2.ts
@@ -9,6 +9,6 @@ export default [
     [[2, 4]]
   ),
   new Talent('Endurance Training', 'spell_nature_reincarnation', ['Increases the health of your pets by ', '%.'], 3, [[5, 10, 15]]),
-  new Talent('Unleashed Fury', 'ability_bullrush', ['Increases the damage done by your pets by ', '%.'], 5, [[2, 4, 6, 8, 10]]),
+  new Talent('Unleashed Fury', 'ability_bullrush', ['Increases the damage done by your pets by ', '%.'], 5, [[4, 8, 12, 16, 20]]),
   new Talent('Aspect Mastery', 'spell_holy_blessingofagility', ['Increases the effect of your Aspects by ', '%.'], 2, [[10, 20]]),
 ]
