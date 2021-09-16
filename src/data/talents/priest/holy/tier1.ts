ea9777bf7d88babf11322671d3fafecd377caf2c
Djimo van Berlo <djimovanberlo@gmail.com>
2021-09-16 21:33:47 +0200

    Update tier1.ts

--- src/data/talents/priest/holy/tier1.ts
+++ src/data/talents/priest/holy/tier1.ts
@@ -8,7 +8,7 @@ export default [
     2,
     [[35, 70]]
   ),
-  new Talent('Improved Renew', 'spell_holy_renew', ['Increases the amount healed by your Renew spell by ', '%.'], 3, [[5, 10, 15]]),
+  // spell armor
   new Talent('Holy Specialization', 'spell_holy_sealofsalvation', ['Increases the critical effect chance of your Holy spells by ', '%.'], 5, [
     [1, 2, 3, 4, 5],
   ]),
