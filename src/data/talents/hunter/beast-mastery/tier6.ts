fac61df3271f6d61ab6e5869b8e98f1aadb00555
Djimo van Berlo <djimovanberlo@gmail.com>
2021-09-16 21:12:30 +0200

    Update tier6.ts

--- src/data/talents/hunter/beast-mastery/tier6.ts
+++ src/data/talents/hunter/beast-mastery/tier6.ts
@@ -6,7 +6,7 @@ export default [
   new Talent(
     'Frenzy',
     'inv_misc_monsterclaw_03',
-    ['Gives your pet a ', '% chance to gain 50% attack speed increase for 8 sec after dealing a critical strike.'],
+    ['Gives your pet a ', '% chance to gain 30% attack speed increase for 8 sec after dealing a critical strike.'],
     5,
     [[20, 40, 60, 80, 100]],
     [{ x: 3, y: 2 }]
