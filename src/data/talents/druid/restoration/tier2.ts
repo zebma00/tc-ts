125361a1510252c61aad45ec654107aca21f188a
Djimo van Berlo <djimovanberlo@gmail.com>
2021-09-16 14:53:51 +0200

    Update tier2.ts

--- src/data/talents/druid/restoration/tier2.ts
+++ src/data/talents/druid/restoration/tier2.ts
@@ -15,7 +15,7 @@ export default [
     'spell_nature_healingwavegreater',
     ['Gives you a ', '% chance to avoid interruption caused by damage while casting the Healing Touch, Regrowth and Tranquility spells.'],
     5,
-    [[14, 28, 42, 56, 70]]
+    [[23, 47, 70]]
   ),
   new Talent('Improved Enrage', 'ability_druid_enrage', ['The Enrage ability now instantly generates ', ' rage.'], 2, [[5, 10]]),
   null,
