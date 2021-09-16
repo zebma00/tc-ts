7cbbc4070c1577acceaa265d7d4a4054c2254f7e
Djimo van Berlo <djimovanberlo@gmail.com>
2021-09-16 22:16:04 +0200

    Update tier1.ts

--- src/data/talents/shaman/restoration/tier1.ts
+++ src/data/talents/shaman/restoration/tier1.ts
@@ -1,7 +1,7 @@
 import { Talent } from '../../Classes'
 
 export default [
-  null,
+  new Talent("Nature's Guidance", 'spell_frost_stun', ['Increases your chance to hit with melee attacks and spells by ', '%.'], 3, [[1, 2, 3]]),
   new Talent('Improved Healing Wave', 'spell_nature_magicimmunity', ['Reduces the casting time of your Healing Wave spell by ', ' sec.'], 5, [
     [0.1, 0.2, 0.3, 0.4, 0.5],
   ]),
