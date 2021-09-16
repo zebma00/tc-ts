7b85d8a35c748b9d609e71530688639e28cb56f0
Djimo van Berlo <djimovanberlo@gmail.com>
2021-09-16 14:40:59 +0200

    Update tier1.ts

--- src/data/talents/warrior/arms/tier1.ts
+++ src/data/talents/warrior/arms/tier1.ts
@@ -6,9 +6,9 @@ export default [
   new Talent(
     'Improved Rend',
     'ability_gouge',
-    ['Increases the damage bleed damage done by your Rend ability by ', '%.'],
+    ['Increases the damage bleed damage done by your Rend ability by ', '% and increases its duration by ', ' sec.'],
     3,
-    [[15, 25, 35]],
+    [[15, 25, 35], [3, 6, 9]],
     [{ x: null, y: null }],
     [{ x: 2, y: 2 }],
     null,
