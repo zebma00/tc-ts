b0ed261c55c5a9bdc1dcd5e50f5a2681d8644cd9
Djimo van Berlo <djimovanberlo@gmail.com>
2021-09-16 21:25:39 +0200

    Update tier2.ts

--- src/data/talents/hunter/marksmanship/tier2.ts
+++ src/data/talents/hunter/marksmanship/tier2.ts
@@ -4,7 +4,12 @@ export default [
   null,
   new Talent('Efficiency', 'spell_frost_wizardmark', ['Reduces the Mana cost of your Shots and Stings by ', '%.'], 5, [[2, 4, 6, 8, 10]]),
   null,
-  new Talent('Improved Concussive Shot', 'spell_frost_stun', ['Gives your Concussive Shot a ', '% chance to stun the target for 3 sec.'], 5, [
-    [4, 8, 12, 16, 20],
-  ]),
+  new Talent(
+  'Mortal Shots',
+  'ability_piercedamage',
+  ['Increases your ranged critical strike damage bonus by ', '%.'],
+  5,
+  [[6, 12, 18, 24, 30]],
+  [{ x: 0, y: 2 }]
+  )
 ]
