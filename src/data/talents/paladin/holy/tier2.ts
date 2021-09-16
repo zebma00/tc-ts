d35f5fa24cea49c18ca4d6164ca1af666ea57f1a
Djimo van Berlo <djimovanberlo@gmail.com>
2021-09-16 21:45:11 +0200

    Create tier2.ts

--- src/data/talents/paladin/holy/tier2.ts
+++ src/data/talents/paladin/holy/tier2.ts
@@ -3,7 +3,7 @@ import { Talent } from '../../Classes'
 export default [
     new Talent('Healing Light', 'spell_holy_holybolt', ['Increases the amount healed by your Flash or Light and Holy Light spells by ', '%.'], 3, [
     [4, 8, 12],
-  ]),,
+  ]),
   new Talent(
     'Spiritual Focus',
     'spell_arcane_blink',
