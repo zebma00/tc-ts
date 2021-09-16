68ef74d9116cdce54d7a48fff6232e589fa10801
Djimo van Berlo <djimovanberlo@gmail.com>
2021-09-16 21:59:10 +0200

    Update tier4.ts

--- src/data/talents/shaman/enhancement/tier4.ts
+++ src/data/talents/shaman/enhancement/tier4.ts
@@ -10,15 +10,6 @@ export default [
     [[10, 15, 20, 25, 30]],
     [{ x: 1, y: 1 }]
   ),
-  new Talent(
-    'Ancestral Fortitude',
-    'spell_holy_devotion',
-    ['Increases your armor by ', '% and your chance to block and chance to dodge by ', '%'],
-    5,
-    [
-      [2, 4, 6, 8, 10],
-      [1, 2, 3, 4, 5],
-    ]
-  ),
+  // LS
   null,
 ]
