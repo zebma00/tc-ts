73d9685896cfbd17a1b23a2e33ea559a26a0f417
Djimo van Berlo <djimovanberlo@gmail.com>
2021-11-11 20:29:51 +0100

    Update tier7.ts

--- src/data/talents/druid/feral-combat/tier7.ts
+++ src/data/talents/druid/feral-combat/tier7.ts
@@ -6,7 +6,7 @@ export default [
     'Leader of the Pack',
     'spell_nature_unyeildingstamina',
     [
-      'While in Cat, Bear, or Dire Bear Form, the Leader of the Pack increases ranged and melee critical chance of all party members within 45 yards by 5%.',
+      'While in Cat, Bear, or Dire Bear Form, the Leader of the Pack increases ranged and melee critical chance of all party members within 45 yards by 5%. When a party member gets a melee or ranged critical strike, they are healed for 2% of their total Health. 6 sec cooldown.',
     ],
     1,
     [[]]
