523ed31734b27a69ce45637df07efbc97a576dbf
Djimo van Berlo <djimovanberlo@gmail.com>
2021-11-11 20:26:10 +0100

    Update tier6.ts

--- src/data/talents/rogue/sublety/tier6.ts
+++ src/data/talents/rogue/sublety/tier6.ts
@@ -3,6 +3,6 @@ import { Talent } from '../../Classes'
 export default [
   null,
   null,
-  new Talent('Deadliness', 'inv_weapon_crossbow_11', ['Increases your attack power by ', '%.'], 5, [[2, 4, 6, 8, 10]]),
+  new Talent('Deadliness', 'inv_weapon_crossbow_11', ['Increases your attack power against bleeding targets by ', '%.'], 5, [[4, 8, 12, 16, 20]]),
   null,
 ]
