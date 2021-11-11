592d83cfef182ebb91cfb3fa3675ea27570c421e
Djimo van Berlo <djimovanberlo@gmail.com>
2021-11-11 20:36:48 +0100

    Update tier6.ts

--- src/data/talents/shaman/restoration/tier6.ts
+++ src/data/talents/shaman/restoration/tier6.ts
@@ -1,13 +1,7 @@
 import { Talent } from '../../Classes'
 
 export default [
-  new Talent(
-    'Improved Chain Heal',
-    'spell_nature_healingwavegreater',
-    ['Your Chain Heal has a ', '% chance to reduce the cast time of your next Healing Wave by 1 sec.'],
-    2,
-    [[15, 30]]
-  ),
+  null,
   null,
   new Talent('Purification', 'spell_frost_wizardmark', ['Increases the effectiveness of your healing spells by ', '%.'], 5, [[2, 4, 6, 8, 10]]),
   null,
