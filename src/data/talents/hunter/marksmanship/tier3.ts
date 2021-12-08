4061b3b949d2e2b94df4447f1a12fce97dab3510
Djimo van Berlo <djimovanberlo@gmail.com>
2021-12-08 21:02:52 +0100

    Update tier3.ts

--- src/data/talents/hunter/marksmanship/tier3.ts
+++ src/data/talents/hunter/marksmanship/tier3.ts
@@ -14,7 +14,7 @@ export default [
     '3 sec cast',
     '6 sec cooldown'
   ),
-  new Talent('Improved Arcane Shot', 'ability_impalingbolt', ['Reduces the cooldown of your Arcane Shot by ', ' sec.'], 3, [[0.4, 0.7, 1]]),
+  new Talent('Improved Arcane Shot', 'ability_impalingbolt', ['Reduces the cooldown of your Arcane Shot by ', ' sec.'], 2, [[0.5, 1]]),
   new Talent('Efficiency', 'spell_frost_wizardmark', ['Reduces the Mana cost of your Shots and Stings by ', '%.'], 5, [[2, 4, 6, 8, 10]]),
   new Talent('Hawk Eye', 'ability_townwatch', ['Increases the range of your ranged weapons by ', ' yards.'], 3, [[2, 4, 6]]),
 ]
