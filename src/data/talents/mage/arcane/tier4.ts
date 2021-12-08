8ef6144bb8fa98c4a0fbb92a2ef310e77dbe8ee8
Djimo van Berlo <djimovanberlo@gmail.com>
2021-12-08 20:50:49 +0100

    Update tier4.ts

--- src/data/talents/mage/arcane/tier4.ts
+++ src/data/talents/mage/arcane/tier4.ts
@@ -6,7 +6,7 @@ export default [
     'spell_shadow_detectlesserinvisibility',
     ['Decreases the mana lost per point of damage when Mana Shield is active by ', '% and increases your chance to resist spells while Mana Shield is active by '. '%.'],
     2,
-    [[10, 20], [2, 4]
+    [[10, 20], [2, 4]]
   ),
   new Talent('Improved Counterspell', 'spell_frost_iceshock', ['Gives your counterspell a ', ' chance to silence the target for 4 sec.'], 2, [
     [50, 100],
