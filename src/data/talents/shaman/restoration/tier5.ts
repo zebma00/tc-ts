5e9ed638cbfce655fb42dcbf4dbb9d63c976aa1f
Djimo van Berlo <djimovanberlo@gmail.com>
2022-01-17 21:30:29 +0100

    quick fix

--- src/data/talents/shaman/restoration/tier5.ts
+++ src/data/talents/shaman/restoration/tier5.ts
@@ -5,13 +5,15 @@ export default [
     'Healing Way',
     'classic_spell_nature_healingway',
     [
-      'Your Healing Wave and Lesser Healing Wave spells have a ',
+      'Your Healing Wave spell has a ',
       '% chance to increase the effect of subsequent Healing Waves on that target by 6% for 15 sec. This effect stacks up to 3 times.',
     ],
     3,
     [[33, 66, 100]]
   ),
   null,
-  new Talent("Nature's Swiftness", 'spell_nature_ravenform', ['When activated, your next Nature spell becomes an instant cast spell.'], 1, [[]]),
+  new Talent("Nature's Swiftness", 'spell_nature_ravenform', ['When activated, your next Nature spell becomes an instant cast spell.'], 1, [
+    [],
+  ]),
   null,
 ]
