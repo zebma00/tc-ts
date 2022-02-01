7b6f4c2e2108e6f4b6ddf7184001e8ab2d477804
Djimo van Berlo <djimovanberlo@gmail.com>
2022-02-01 17:10:53 +0100

    quick update

--- src/data/patch-notes/class-changes/classes/warrior.ts
+++ src/data/patch-notes/class-changes/classes/warrior.ts
@@ -1,8 +1,9 @@
 export default {
-	title: 'Warrior',
-	content: [
-		'Rend base damage increased.',
-		'Thunder Clap can now be used in defensive stance.',
-		'New ability: Victory Rush: Instantly attack the target causing damage. Can only be used within 20 sec after you kill an enemy that yields experience or honor.  Damage is based on your attack power.',
-	],
+  title: 'Warrior',
+  content: [
+    'New ability: Stance Mastery: You retain up to 10 rage points when you change stances. Learned at lvl 20.',
+    'Rend base damage increased.',
+    'Thunder Clap can now be used in defensive stance.',
+    'New ability: Victory Rush: Instantly attack the target causing damage. Can only be used within 20 sec after you kill an enemy that yields experience or honor.  Damage is based on your attack power.',
+  ],
 }
