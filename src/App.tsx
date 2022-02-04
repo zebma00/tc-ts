25c2dd2f61b0de54199f5189cdf95bc99bc3dcef
Djimo van Berlo <djimovanberlo@gmail.com>
2022-02-04 11:50:56 +0100

    hasrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { HashRouter, BrowserRouter as Router, Route } from 'react-router-dom'
 
 import TalentCalculator from './components/talent-calculator'
 import PatchNotes from './components/patch-notes'
@@ -7,16 +7,16 @@ import NavBar from './components/nav-bar'
 import styles from './index.module.css'
 
 const App = () => {
-	return (
-		<div id={styles.app}>
-			<Router>
-				<NavBar />
-				<Route exact path='/' component={PatchNotes} />
-				<Route path='/tc' component={TalentCalculator} />
-				<Route path='/items' component={ExampleItems} />
-			</Router>
-		</div>
-	)
+  return (
+    <div id={styles.app}>
+      <HashRouter>
+        <NavBar />
+        <Route exact path='/' component={PatchNotes} />
+        <Route path='/tc' component={TalentCalculator} />
+        <Route path='/items' component={ExampleItems} />
+      </HashRouter>
+    </div>
+  )
 }
 
 export default App
