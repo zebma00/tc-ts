41bf194ff13df6594d0e51e2f333bee36e1c54f0
Djimo van Berlo <djimovanberlo@gmail.com>
2022-02-09 22:12:45 +0100

    update

--- src/data/patch-notes/class-changes/classes/hunter.ts
+++ src/data/patch-notes/class-changes/classes/hunter.ts
@@ -4,7 +4,7 @@ export default {
     'Hunters gain an additional stable slot.',
     "New ability: Aspect of the Viper: The hunter takes on the aspects of a viper, regenerating mana equal to up to 55% of his/her Intellect plus 35% of his level every 5 sec. The lower the hunter's current mana, the more mana will be regenerated.  Only one Aspect can be active at a time. Learned at lvl 60.",
     'Mend pet is now instant cast and has 40 yard range. It is still a heal over time effect.',
-    'Aspect of the Beast now also greatly reduces the range beasts will attack you and your pet.',
+    'Aspect of the Beast now also greatly reduces the range hostile beasts will attack you and your pet.',
     "All pets will now receive a percentage of their master's Health, Mana, armor, spell resistances, and their attack power and spell damage is increased by a percentage of their master’s attack power.",
   ],
 }
--- src/data/patch-notes/class-changes/classes/priest.ts
+++ src/data/patch-notes/class-changes/classes/priest.ts
@@ -1,9 +1,9 @@
 export default {
   title: 'Priest',
   content: [
-    'Feedback (Human) now does shadow damage and burns mana when dispelled.',
+    'Feedback (Human) now also does shadow damage and burns mana when dispelled.',
     'Elune’s Grace (Night Elf) now reduces the chance you are hit by melee or ranged attacks by 25% for 15 sec.',
-    'Starshards (Night Elf) damage increased and has a 30 sec cooldown.',
+    'Starshards (Night Elf) is now instant cast, lasts 15 sec and has a 30 sec cooldown.',
     'Mind Soothe can no longer be resisted.',
     'Reduced the mana cost of Power word: Fortitude, Prayer of Fortitude, Divine Spirit, Prayer of Spirit, Shadow Protection and Prayer of Shadow Protection.',
   ],
