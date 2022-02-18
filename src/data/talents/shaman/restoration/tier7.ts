236d986680fe11e382144217324022c1fc2ebc0e
Djimo van Berlo <djimovanberlo@gmail.com>
2022-02-19 00:27:29 +0100

    update

--- src/data/talents/shaman/restoration/tier7.ts
+++ src/data/talents/shaman/restoration/tier7.ts
@@ -3,10 +3,10 @@ import { Talent } from '../../Classes'
 const tier7 = [
   null,
   new Talent(
-    'Mana Tide Totem',
+    'Torrent Totem',
     'spell_frost_summonwaterelemental',
     [
-      'Summons a torrent totem at the feet of the caster with 5 health for 12 sec that restores 170 mana to all party members within 20 yards every 3 sec.',
+      'Summons a torrent totem at the feet of the caster with 5 health for 12 sec that restores 170 mana and health to all party members within 20 yards every 3 sec.',
     ],
     1,
     [[1]],
@@ -15,7 +15,9 @@ const tier7 = [
     '20 Mana',
     null,
     'Instant',
-    '5 min cooldown'
+    '5 min cooldown',
+    null,
+    { isNew: true, isChanged: false, isMoved: false }
   ),
   null,
   null,
