75aa2d59f01cd9933aad09589a829ed8e30f5870
Djimo van Berlo <djimovanberlo@gmail.com>
2023-01-21 18:04:17 +0100

    quick nerf

--- src/data/talents/warrior/protection/tier5.ts
+++ src/data/talents/warrior/protection/tier5.ts
@@ -17,7 +17,7 @@ const tier5 = [
     ['D2'],
     { isNew: false, isChanged: true, isMoved: false }
   ),
-  new Talent('Bad Temper', 'ability_warrior_endlessrage', ['Gain ', ' rage when you take damage. 3 sec cooldown.'], 2, [[5, 1]], null, null, null, null, null, null, null, {
+  new Talent('Bad Temper', 'ability_warrior_endlessrage', ['Gain ', ' rage when you take damage. 5 sec cooldown.'], 2, [[5, 1]], null, null, null, null, null, null, null, {
     isNew: true,
     isChanged: false,
     isMoved: false,
