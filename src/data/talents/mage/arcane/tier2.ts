45222ee44f4cae2e274b52041b1fb6026b3a7616
Djimo van Berlo <djimovanberlo@gmail.com>
2023-02-03 20:57:37 +0100

    wizardry update

--- src/data/talents/mage/arcane/tier2.ts
+++ src/data/talents/mage/arcane/tier2.ts
@@ -52,12 +52,9 @@ const tier2 = [
   new Talent(
     'Wizardry',
     'ability_mage_potentspirit',
-    [
-      'Your Arcane Missiles, Fireball, Frostbolt and Scorch increase the damage done by your Arcane Missiles, Fireball, Frostbolt and Scorch by ',
-      '% for 30 sec. This effect stacks up to 5 times. This effect is lost when casting the same spell twice in a row.',
-    ],
-    2,
-    [[2, 4]],
+    ['When you cast a damaging spell, your spell damage is increased by ', '% for 30 sec. This effect stacks up to 5 times. This effect is lost when casting the same spell twice in a row.'],
+    3,
+    [[1, 2, 3]],
     null,
     null,
     null,
