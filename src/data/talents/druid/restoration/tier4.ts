504f9fccc2557ad6157404f086f19adee80a710b
Djimo van Berlo <djimovanberlo@gmail.com>
2023-04-23 16:49:03 +0200

    druid

--- src/data/talents/druid/restoration/tier4.ts
+++ src/data/talents/druid/restoration/tier4.ts
@@ -2,27 +2,17 @@ import { Talent } from '../../Classes'
 
 const tier4 = [
   null,
-  new Talent(
-    'Tranquil Spirit',
-    'spell_holy_elunesgrace',
-    ['Reduces the mana cost of your healing spells by ', '%.'],
-    5,
-    [[2, 4, 6, 8, 10]],
-    [{ x: null, y: null }],
-    [{ x: 6, y: 1 }],
-    null,
-    null,
-    null,
-    null,
-    ['D3']
-  ),
+  new Talent('Tranquil Spirit', 'spell_holy_elunesgrace', ['Reduces the mana cost of your healing spells by ', '%.'], 5, [[2, 4, 6, 8, 10]], [{ x: null, y: null }], [{ x: 6, y: 1 }], null, null, null, null, ['D3']),
   null,
   new Talent(
-    'Living Seed',
-    'ability_druid_empoweredrejuvination',
-    ['Causes your Rejuvenation and Regrowth spells to apply the Living Seed effect. The Living Seed effect Increases healing received by the target by ', '%. This effect does not stack.'],
+    'Improved Innervate',
+    'spell_nature_lightning',
+    ['Increases the effect of your Innervate by ', '% when cast on self. You gain mana regeneration equal to ', '% of the Innervate effect when cast on another target.'],
     2,
-    [[5, 10]],
+    [
+      [15, 30],
+      [15, 30],
+    ],
     null,
     null,
     null,
--- src/data/talents/druid/restoration/tier6.ts
+++ src/data/talents/druid/restoration/tier6.ts
@@ -1,5 +1,28 @@
 import { Talent } from '../../Classes'
 
-const tier6 = [null, null, new Talent('Improved Regrowth', 'spell_nature_resistnature', ['Increases the critical effect chance of your Regrowth spell by ', '%.'], 5, [[10, 20, 30, 40, 50]]), null]
+const tier6 = [
+  new Talent(
+    'Living Seed',
+    'ability_druid_empoweredrejuvination',
+    ['Causes your Rejuvenation and Regrowth spells to apply the Living Seed effect. The Living Seed effect Increases healing received by the target by ', '%. This effect does not stack.'],
+    2,
+    [[5, 10]],
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    {
+      isNew: true,
+      isChanged: false,
+      isMoved: false,
+    }
+  ),
+  null,
+  new Talent('Improved Regrowth', 'spell_nature_resistnature', ['Increases the critical effect chance of your Regrowth spell by ', '%.'], 5, [[10, 20, 30, 40, 50]]),
+  null,
+]
 
 export default tier6
