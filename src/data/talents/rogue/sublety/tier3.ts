bd2a88f549494d28284842ae2b3b500eb9287c16
Djimo van Berlo <djimovanberlo@gmail.com>
2023-07-04 20:18:06 +0200

    sub updates

--- src/data/talents/rogue/sublety/tier3.ts
+++ src/data/talents/rogue/sublety/tier3.ts
@@ -1,13 +1,11 @@
 import { Talent } from '../../Classes'
 
 const tier3 = [
-  new Talent('Initiative', 'spell_shadow_fumble', ['Gives you a ', '% chance to add an additional combo point to your target when using your Ambush, Garrote or Cheap Shot ability.'], 3, [
-    [25, 50, 75],
-  ]),
+  new Talent('Initiative', 'spell_shadow_fumble', ['Gives you a ', '% chance to add an additional combo point to your target when using your Ambush, Garrote or Cheap Shot ability.'], 3, [[25, 50, 75]]),
   new Talent(
     'Ghostly Strike',
     'spell_shadow_curse',
-    ['A strike that deals 125% weapon damage and increases your chance to dodge by 15% for 7 sec. Awards 1 combo point. Cooldown 20 seconds'],
+    ['A strike that deals 125% weapon damage and increases your chance to dodge by 25% and causes your attacks to ignore 25% armor for 6 sec. Awards 1 combo point.'],
     1,
     [[]],
     null,
@@ -15,7 +13,9 @@ const tier3 = [
     '40 Energy',
     'Melee Range',
     'Instant',
-    '20 sec cooldown'
+    '20 sec cooldown',
+    null,
+    { isNew: false, isChanged: true, isMoved: false }
   ),
   new Talent('Improved Ambush', 'ability_rogue_ambush', ['Increases the critical strike chance of your Ambush ability by ', '%.'], 3, [[15, 30, 45]]),
   new Talent(
--- src/data/talents/rogue/sublety/tier6.ts
+++ src/data/talents/rogue/sublety/tier6.ts
@@ -1,28 +1,5 @@
 import { Talent } from '../../Classes'
 
-const tier6 = [
-  null,
-  null,
-  new Talent(
-    'Deadliness',
-    'inv_weapon_crossbow_11',
-    ['Increases your attack power against bleeding targets by ', '%.'],
-    5,
-    [[4, 8, 12, 16, 20]],
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    {
-      isNew: false,
-      isChanged: true,
-      isMoved: false,
-    }
-  ),
-  null,
-]
+const tier6 = [null, null, new Talent('Deadliness', 'inv_weapon_crossbow_11', ['Increases your attack power by ', '%.'], 5, [[2, 4, 6, 8, 10]]), null]
 
 export default tier6
