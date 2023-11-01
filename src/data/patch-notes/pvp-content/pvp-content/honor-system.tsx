90e0f4adc4abb212c82ffc511d196b44f20bb41d
Djimo van Berlo <djimovanberlo@gmail.com>
2023-11-01 19:16:46 +0100

    minor updates

--- src/data/patch-notes/pvp-content/pvp-content/honor-system.tsx
+++ src/data/patch-notes/pvp-content/pvp-content/honor-system.tsx
@@ -1,9 +1,4 @@
 export default {
   title: 'Honor system',
-  content: [
-    'Battleground Marks of Honor can be traded in for Alliance or Horde Marks of Honor.',
-    'World mini bosses also drop an Alliance or Horde Mark of Honor.',
-    'Alliance or Horde Marks of Honor can be traded in at new capital city vendors in exchange for PvP equipment. These items can be acquired regardless of rank. This offers an alternative to conventional ranked PvP gear, although epic rank gear should still be better in most cases.',
-    'Additional PvP items and sets have been added and changed to better fit various classes and specs.',
-  ],
+  content: ['World mini bosses also drop an Alliance or Horde Mark of Honor.', 'Additional PvP items and sets have been added and changed to better fit various classes and specs.'],
 }
