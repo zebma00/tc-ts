4795836caea7c320c1114b02aa3816aa1bb4075f
Djimo van Berlo <djimovanberlo@gmail.com>
2023-11-03 11:11:30 +0100

    updates

--- src/data/talents/druid/feral-combat/tier7.ts
+++ src/data/talents/druid/feral-combat/tier7.ts
@@ -6,7 +6,7 @@ const tier7 = [
     'Leader of the Pack',
     'spell_nature_unyeildingstamina',
     [
-      'While in Cat, Bear, or Dire Bear Form, the Leader of the Pack increases ranged and melee critical chance of all party members within 30 yards by 5%. When a party member gets a melee or ranged critical strike, their attack speed is increased by 10% for 6 sec.',
+      'While in Cat, Bear, or Dire Bear Form, the Leader of the Pack increases ranged and melee critical chance of all party members within 45 yards by 5%. When a party member gets a melee or ranged critical strike, their attack speed is increased by 10% for 6 sec.',
     ],
     1,
     [[]],
--- src/data/talents/mage/arcane/tier3.ts
+++ src/data/talents/mage/arcane/tier3.ts
@@ -1,7 +1,11 @@
 import { Talent } from '../../Classes'
 
 const tier3 = [
-  new Talent('Magic Attunement', 'spell_nature_abolishmagic', ['Increases the effect of your Amplify Magic and Dampen Magic spells by ', '%.'], 2, [[25, 50]]),
+  new Talent('Magic Attunement', 'spell_nature_abolishmagic', ['Increases the effect of your Amplify Magic and Dampen Magic spells by ', '%.'], 2, [[0.5, 1]], null, null, null, null, null, null, null, {
+    isNew: false,
+    isChanged: true,
+    isMoved: false,
+  }),
   new Talent('Improved Arcane Explosion', 'spell_nature_wispsplode', ['Increases the critical strike chance of your Arcane Explosion spell by ', '%.'], 3, [[2, 4, 6]]),
   new Talent('Mana Tap', 'spell_frost_manarecharge', ['When activated, reduces the mana cost of your spells by 50% for 6 sec.'], 1, [[]], null, [{ x: 4, y: 2 }], null, null, 'Instant', '2 min cooldown', ['D2'], {
     isNew: true,
